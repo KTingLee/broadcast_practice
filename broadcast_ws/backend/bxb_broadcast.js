0252c85e07ac2cbc26556af03f8301fee285c85f
JoeLee <joelee@bxb.tw>
2021-01-27 17:53:18 +0800

    feat(broadcast_ws): 將前端的聲音，利用 ws 傳到後端，後端再將其輸出到聲音設備

--- broadcast_ws/backend/bxb_broadcast.js
+++ broadcast_ws/backend/bxb_broadcast.js
@@ -5,20 +5,20 @@ const dgram = require('dgram')
 const util = require('util')
 
 const debug = require('debug')('bxb:broadcast')
-const rtAudio = new RtAudio(RtAudioApi.LINUX_ALSA)
+const rtAudio = new RtAudio(RtAudioApi.MACOSX_CORE)
 
 const BUFFER_SIZE = 512
 
 rtAudio.openStream(
   {
     deviceId: rtAudio.getDefaultOutputDevice(),
-    nChannels: 2,
+    nChannels: 1,
     firstChannel: 0
   },
   null,
   RtAudioFormat.RTAUDIO_SINT16,
   48000,
-  BUFFER_SIZE,
+  1024,
   'MyStream',
   null,
   null,
@@ -48,16 +48,50 @@ Broadcast.prototype.start = function (obj) {
   // 2. 依照提供的 type 產生對應的 socket
   if (obj.type === 'ws') {
     this.sck = new WebSocket.Server({ port: 8000 })
-    this.sck.on('connection', socket => {
-      console.log(socket);
+    this.sck.on('connection', (socket, req) => {
+      console.log(req.socket.remoteAddress);  // 獲得使用者 ip
       rtAudio.start()
       // 3. 接收資料前再次確認 ip
       socket.on('message', data => {
-        console.log(data);
+        chunkStream.write(data)
       })
     })
   }
 }
+
+// NOTE: 因為丟到底層的 size = buffer * 4，不知道 udp 會收到多長的情況下做一個 Chunk 來切送到底層的資料
+const { PassThrough } = require('stream').PassThrough
+const Transform = require('stream').Transform
+
+// NOTE: chunk trans stream，用來把進來的資料切塊
+function Chunk (size) {
+  this.splitSize = size
+  this.buffer = Buffer.alloc(0)
+  Transform.call(this)
+}
+
+util.inherits(Chunk, Transform)
+
+Chunk.prototype._transform = function (chunk, encoding, cb) {
+  this.buffer = Buffer.concat([this.buffer, chunk])
+  while (this.buffer.length > this.splitSize) {
+    let chunk = this.buffer.slice(0, this.splitSize)
+    this.push(chunk)
+    this.buffer = this.buffer.slice(this.splitSize)
+  }
+  cb()
+}
+
+rtAudio.outputVolume = 1
+
+// NOTE: 把 chunk pipe 給 passthrough，passthrough on data 時的資料 size 就會是我們要的 size
+const chunkStream = new Chunk(BUFFER_SIZE * 4)
+const passStream = new PassThrough()
+chunkStream.pipe(passStream)
+passStream.on('data', data => {
+  rtAudio.write(data)
+})
+
 
 util.inherits(Broadcast, EventEmitter)
 module.exports = new Broadcast()
