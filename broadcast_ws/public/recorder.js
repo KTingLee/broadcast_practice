05aed199606d31cc7d2de05dd0f62853f373d96c
JoeLee <joelee@bxb.tw>
2021-01-29 12:06:50 +0800

    refactory(recorder.js): 加上一些註解，方便了解 recorder.js 運作
    結論：好像不需要用到 recorder.js 套件，因為我們只是要把前端的聲音送到後端，用瀏覽器提供的物件就行惹（ScriptProcessor）？

--- broadcast_ws/public/recorder.js
+++ broadcast_ws/public/recorder.js
@@ -4,6 +4,7 @@ var Recorder = function(source, cfg){
   var config = cfg || {};
   var bufferLen = config.bufferLen || 4096;
   this.context = source.context;
+  // ScriptProcessor 可以自定義「如何處理聲音訊號」的節點
   this.node = (this.context.createScriptProcessor ||
                this.context.createJavaScriptNode).call(this.context,
                                                        bufferLen, 2, 2);
@@ -22,26 +23,26 @@ var Recorder = function(source, cfg){
   var recording = false,
     currCallback;
 
-  
+  // 在 index.html 中，new Recorder 後，其實前端的聲音一直送到 this.node (也就是一直觸發 onaudioprocess)
   this.node.onaudioprocess = function(e){
+    // console.log(e);
     if (!recording) return;
     // mediaRecorder
-    // console.log(e.inputBuffer.getChannelData(0));
-    console.log(e);
+    console.log(e.inputBuffer.numberOfChannels);  // 看一下傳入 recorder 的 sound 有幾個聲道
     
-    const left = e.inputBuffer.getChannelData(0);
+    const left = e.inputBuffer.getChannelData(1);
     broadcast_socket.send(convertFloat32ToInt16(left))
     console.log(convertFloat32ToInt16(left));
     
     // window.Stream.write(convertFloat32ToInt16(left));
 
-    worker.postMessage({
-      command: 'record',
-      buffer: [
-        e.inputBuffer.getChannelData(0),
-        e.inputBuffer.getChannelData(0)
-      ]
-    });
+    // worker.postMessage({
+    //   command: 'record',
+    //   buffer: [
+    //     e.inputBuffer.getChannelData(0),
+    //     e.inputBuffer.getChannelData(0)
+    //   ]
+    // });
   }
 
 
@@ -64,13 +65,10 @@ var Recorder = function(source, cfg){
 
   this.record = function(){
     recording = true;
-    
   }
 
   this.stop = function(){
     recording = false;
-    broadcast_socket.send('停止錄音')
-
   }
 
   this.clear = function(){
@@ -92,7 +90,7 @@ var Recorder = function(source, cfg){
     });
   }
 
-  source.connect(this.node);
+  source.connect(this.node);  // index.html 中，new Recorder 時，會傳入前端的 sound stream，而該 stream 會與 Recorder 物件連結
   this.node.connect(this.context.destination);    //this should not be necessary
 };
 
